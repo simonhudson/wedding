eb8240a190bbb5004312de538cdddf22509b99ba
Simon Hudson <simon.hudson@experian.com>
2017-07-13 13:22:32 +0100

    Update seating page

--- seating.php
+++ seating.php
@@ -1,7 +1,7 @@
 <?php include('layout/precontent.inc.php'); ?>
 
 <?php foreach (getSeating() as $table): ?>
-	<?= $table['name']; ?>
+	<?= $table['table_name']; ?>
 <?php endforeach; ?>
 
 <?php include('layout/postcontent.inc.php'); ?>
