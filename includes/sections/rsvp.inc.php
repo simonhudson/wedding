38354fd4684c071e0f75a2ebb0fac856d7f12e72
Simon Hudson <simon.hudson@experian.com>
2017-07-20 09:08:19 +0100

    RSVP section

--- includes/sections/rsvp.inc.php
+++ includes/sections/rsvp.inc.php
@@ -4,5 +4,21 @@
 		<legend class="visuallyhidden">RSVP</legend>
 		<label class="visuallyhidden">Search for your name</label>
 		<input class="js-live-search__input" id="rsvp-name" placeholder="Search for your name" type="text" />
+		<fieldset>
+			<legend>Will you be able to join us?</legend>
+			<input id="attendance-yes" name="rsvp-attendance" type="radio" value="yes" />
+			<label for="attendance-yes">Yes, I will be there</label>
+			<input id="attendance-no" name="rsvp-attendance" type="radio" value="no" />
+			<label for="attendance-no">No, I will not be able to attend</label>
+		</fieldset>
+		<fieldset>
+			<legend>What would you like to eat?</legend>
+			<?php foreach (getMeals() as $meal): ?>
+				<input id="meal-<?= $meal['meal_id']; ?>" name="rsvp-meal" type="radio" value="<?= $meal['meal_id']; ?>" />
+				<label for="meal-<?= $meal['meal_id']; ?>"><?= $meal['meal_description']; ?></label>
+			<?php endforeach; ?>
+		</fieldset>
+		<label for="rvsp-message">Any other message for the Bride &amp; Groom?</label>
+		<textarea id="rsvp-message"></textarea>
 	</fieldset>
 </form>
