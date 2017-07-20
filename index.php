47bd867df2635984a98132346c0f0d536f58f550
Simon Hudson <simon.hudson@experian.com>
2017-07-20 07:40:19 +0100

    Add section includes

--- index.php
+++ index.php
@@ -1,57 +1,14 @@
 <?php include('layout/precontent.inc.php'); ?>
 
 	<div class="hero">
-
 	</div>
 
-	<section class="section" id="day">
-		<div class="wrap section__wrap">
-			<div class="section__item">
-				<h2>The day</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p><a href="#top">Back to top</a></p>
-			</div>
-		</div>
-	</section>
-
-	<section class="section" id="evening">
-		<div class="wrap section__wrap">
-			<div class="section__item">
-				<h2>The evening</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p><a href="#top">Back to top</a></p>
-			</div>
-		</div>
-	</section>
-
-	<section class="section" id="travel">
-		<div class="wrap section__wrap">
-			<div class="section__item">
-				<h2>Travel &amp; accommodation</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p><a href="#top">Back to top</a></p>
-			</div>
-		</div>
-	</section>
-
-	<section class="section" id="rsvp">
-		<div class="wrap section__wrap">
-			<div class="section__item">
-				<h2>RSVP</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p><a href="#top">Back to top</a></p>
-			</div>
-		</div>
-	</section>
-
-	<section class="section" id="contact">
-		<div class="wrap section__wrap">
-			<div class="section__item">
-				<h2>Contact us</h2>
-				<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
-				<p><a href="#top">Back to top</a></p>
-			</div>
-		</div>
-	</section>
+	<?php
+	include('includes/sections/day.inc.php');
+	include('includes/sections/evening.inc.php');
+	include('includes/sections/travel.inc.php');
+	include('includes/sections/rsvp.inc.php');
+	include('includes/sections/contact.inc.php');
+	?>
 
 <?php include('layout/postcontent.inc.php'); ?>
