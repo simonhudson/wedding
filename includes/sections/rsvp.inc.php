b24b357b1607262cbf58669acbcb071cea13596c
Simon Hudson <simon.hudson@experian.com>
2017-07-20 07:44:04 +0100

    RSVP section

--- includes/sections/rsvp.inc.php
+++ includes/sections/rsvp.inc.php
@@ -5,5 +5,14 @@
 			<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.</p>
 			<p><a href="#top">Back to top</a></p>
 		</div>
+		<div class="section__item">
+			<form class="rsvp js-live-search">
+				<fieldset>
+					<legend class="visuallyhidden">RSVP</legend>
+					<label class="visuallyhidden">Search for your name</label>
+					<input class="js-live-search__input" id="rsvp-name" placeholder="Search for your name" type="text" />
+				</fieldset>
+			</form>
+		</div>
 	</div>
 </section>
