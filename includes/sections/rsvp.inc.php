08b3ef77bd3140f8f28fdf895045f895630bca1e
Simon Hudson <simon.hudson@experian.com>
2017-07-20 09:15:13 +0100

    rsvp form styles

--- includes/sections/rsvp.inc.php
+++ includes/sections/rsvp.inc.php
@@ -6,16 +6,22 @@
 		<input class="js-live-search__input" id="rsvp-name" placeholder="Search for your name" type="text" />
 		<fieldset>
 			<legend>Will you be able to join us?</legend>
-			<input id="attendance-yes" name="rsvp-attendance" type="radio" value="yes" />
-			<label for="attendance-yes">Yes, I will be there</label>
-			<input id="attendance-no" name="rsvp-attendance" type="radio" value="no" />
-			<label for="attendance-no">No, I will not be able to attend</label>
+			<div class="input-row">
+				<input id="attendance-yes" name="rsvp-attendance" type="radio" value="yes" />
+				<label for="attendance-yes">Yes, I will be there</label>
+			</div>
+			<div class="input-row">
+				<input id="attendance-no" name="rsvp-attendance" type="radio" value="no" />
+				<label for="attendance-no">No, I will not be able to attend</label>
+			</div>
 		</fieldset>
 		<fieldset>
 			<legend>What would you like to eat?</legend>
 			<?php foreach (getMeals() as $meal): ?>
-				<input id="meal-<?= $meal['meal_id']; ?>" name="rsvp-meal" type="radio" value="<?= $meal['meal_id']; ?>" />
-				<label for="meal-<?= $meal['meal_id']; ?>"><?= $meal['meal_description']; ?></label>
+				<div class="input-row">
+					<input id="meal-<?= $meal['meal_id']; ?>" name="rsvp-meal" type="radio" value="<?= $meal['meal_id']; ?>" />
+					<label for="meal-<?= $meal['meal_id']; ?>"><?= $meal['meal_description']; ?></label>
+				</div>
 			<?php endforeach; ?>
 		</fieldset>
 		<label for="rvsp-message">Any other message for the Bride &amp; Groom?</label>
