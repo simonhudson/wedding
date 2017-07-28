5c6c896026a280bd1a04efe6418e412d47dae45a
Simon Hudson <simon.hudson@experian.com>
2017-07-28 07:00:10 +0100

    Main nav link updates

--- includes/main-nav.inc.php
+++ includes/main-nav.inc.php
@@ -1,7 +1,7 @@
 <nav class="main-nav header__nav js-main-nav">
 	<ul class="main-nav__list">
 		<?php foreach($sections as $section): ?>
-			<li class="main-nav__item"><a class="main-nav__link" href="#<?= $section->slug; ?>"><?= $section->title; ?></a></li>
+			<li class="main-nav__item"><a class="main-nav__link" href="<?= $root; ?>#<?= $section->slug; ?>"><?= $section->title; ?></a></li>
 		<?php endforeach; ?>
 	</ul>
 </nav>
