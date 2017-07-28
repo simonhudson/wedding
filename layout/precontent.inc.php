f7cde950bafc7f45af5023dc06d4c659385cd93b
Simon Hudson <simon.hudson@experian.com>
2017-07-28 07:14:26 +0100

    logo link styles

--- layout/precontent.inc.php
+++ layout/precontent.inc.php
@@ -17,7 +17,7 @@
 		<header class="header">
 			<div class="wrap header__wrap">
 				<div class="header__logo">
-					<a href="<?= $root; ?>">
+					<a class="header__link" href="<?= $root; ?>">
 						<img alt="" src="http://via.placeholder.com/200x75" />
 					</a>
 				</div>
