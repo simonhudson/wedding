d02075343784b5a517db182b4c1e598486b5bf6c
Simon Hudson <hellosimonhudson+github@gmail.com>
2017-08-21 08:37:22 +0100

    Hero work

--- includes/hero.inc.php
+++ includes/hero.inc.php
@@ -1,8 +1,3 @@
 <div class="hero">
-	<div class="wrap section__wrap hero__text">
-		<h1 class="hero__title">
-			<span><?= $sitename; ?></span>
-			<span><?= formatDateTime(); ?></span>
-		</h1>
-	</div>
+	
 </div>
